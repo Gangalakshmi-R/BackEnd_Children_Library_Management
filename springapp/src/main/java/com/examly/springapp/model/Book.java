0e76909473971ac42428f34b9aa5c3d52fa51832
Gangalakshmi-R <glr16.engineer@gmail.com>
2026-02-03 13:06:34 +0530

    commit

--- springapp/src/main/java/com/examly/springapp/model/Book.java
+++ springapp/src/main/java/com/examly/springapp/model/Book.java
@@ -10,7 +10,7 @@ import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
-@Data
+// @Data
 public class Book {
 
     @Id
@@ -23,5 +23,48 @@ public class Book {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private BookCategory bookCategory;
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public BookCategory getBookCategory() {
+        return bookCategory;
+    }
+
+    public void setBookCategory(BookCategory bookCategory) {
+        this.bookCategory = bookCategory;
+    }
+
+    
+
 
 }
